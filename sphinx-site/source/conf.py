7d62a370dff8738a96c37f12e53384d75e33f81a
V Prakash <68439460+Pravas20@users.noreply.github.com>
2025-11-13 01:39:42 +0530

    Update conf.py

--- sphinx-site/source/conf.py
+++ sphinx-site/source/conf.py
@@ -6,25 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'prakashsite'
-copyright = '2025, v prakash'
-author = 'v prakash'
+project = 'Mini Sphinx Site'
+copyright = '2025,Prakash'
+author = 'V Prakash'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
+extensions = ['myst_parser']
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+
+
+
 
